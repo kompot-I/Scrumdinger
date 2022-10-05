336efe3778b57ecc91ea965ded08ea2875049e0a
Nnikkita <42681434+kompot-I@users.noreply.github.com>
2022-10-05 12:26:48 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Nikita  Zubov on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
